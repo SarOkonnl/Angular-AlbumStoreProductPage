2017293cc004445290b6183d7ec29383440885bd
Brian Spence <brspence@cisco.com>
2018-12-30 22:31:27 -0600

    Add interfaces

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
+import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs/Observable';
+import { Album } from './album';
 
 
 @Injectable()
@@ -12,7 +14,7 @@ export class ProductService {
     private _http: Http
   ) { }
 
-  getAlbum(id: number) {
-    return this._http.get(this._albumUrl).map((response: Response) => response.json());
+  getAlbum(id: number): Observable<Album> {
+    return this._http.get(this._albumUrl).map(response => <Album>response.json());
   }
 }
